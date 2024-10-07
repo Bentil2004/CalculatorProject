747ef7ba46cf7f189ffe3889a4db68ae77d6e178
fiifi <fiifiampoma@gmail.com>
2024-10-07 16:32:27 +0000

    Done

--- calcindex.js
+++ calcindex.js
@@ -1,27 +1,27 @@
-const result = document.getElementById('result');
+const result = document.getElementById("result");
 
-function appendToDisplay(input){
-     result.value += input;
+function appendToDisplay(input) {
+  result.value += input;
 }
 
-function clearDisplay(){
-    result.value = '';
+function clearDisplay() {
+  result.value = "";
 }
 
-function calculate(){
-    try {
-        const expression = result.value;
-        const resultValue = eval(expression);
-        result.value = resultValue;
-    } catch (error) {
-        result.value = 'Error';
-    }
+function calculate() {
+  try {
+    const calculation = result.value;
+    const resultValue = eval(calculation);
+    result.value = parseFloat(resultValue).toFixed(2);
+  } catch (error) {
+    result.value = "Error";
+  }
 }
 
 function calculatePercentage() {
-    result.value = (parseFloat(result.value) / 100).toString();
+  result.value = (parseFloat(result.value) / 100).toString();
 }
 
 function changeSign() {
-        result.value = (parseFloat(result.value) * -1).toString();
+  result.value = (parseFloat(result.value) * -1).toString();
 }
